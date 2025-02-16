5267978ba37ebaa77cde533835839720bbefad93
salahcurvasud <khalfaoui__salah@live.fr>
2025-02-16 20:20:32 +0100

    submit changes

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -5,12 +5,19 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\TerrainRepository; // Add this to use TerrainRepository
 
 class DashboardController extends AbstractController
 {
     #[Route('/dashboard', name: 'dashboard')]
-    public function index(): Response
+    public function index(TerrainRepository $terrainRepository): Response
     {
-        return $this->render('base_admin.html.twig');
+        $terrains = $terrainRepository->findBy([], ['id' => 'DESC'], 5); // Fetch recent 5 terrains
+        $totalTerrains = count($terrainRepository->findAll());
+
+        return $this->render('dashboard/index.html.twig', [
+            'terrains' => $terrains,
+            'totalTerrains' => $totalTerrains,
+        ]);
     }
 }
